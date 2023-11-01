a58a3b8cd1416415b317f3e5bd76ae641e9e7237
Engschamm <149345263+Engschamm@users.noreply.github.com>
2023-11-01 07:22:54 +0700

    Add files via upload

---
+++ ConditionalIf/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	//viet chuong trinh nhap vao so nguyen. kiem tra so do nguyen duong hay am. in ra man hinh ket qua 
+//	int a;
+//	printf("nhap a: ");
+//	scanf("%d", &a);
+//	if (a > 0)
+//	{
+//		printf("%d la so nguyen duong \n", a); 
+//		}	
+//	else{
+//		printf("%d la so nguyen am \n", a); 
+//	} 
+	//nhap vao mot so nguyen n. kiem tra n chia het cho 3 hay 5
+//	int n;
+//	printf("nhap n: ");
+//	scanf("%d", &n);
+//	if( n % 3 == 0){
+//		printf("%d chia het cho 3", n);
+//	}else if(n % 5 == 0){
+//			printf("%d chia het cho 5", n); 
+//	}else {
+//		printf("%d khong chia het cho 3 va 5", n); 
+//	} 
+	//nhap vao thang trong nam. in ra so ngay cua thang do
+//	int thang;
+//	printf("nhap thang: ");
+//	scanf("%d", &thang);
+//	if(thang == 2) {
+//		printf("thang 2 co 28 hoac 29 ngay\n"); 
+//	} 
+//	else if(thang == 4 || thang == 6 || thang == 9 || thang == 11){
+//		printf("thang %d co 30 ngay\n", thang);
+//	}
+//		 else {
+//		printf("thang %d co 31 ngay\n", thang); 
+//	}
+	//giai phuong trinh bac 2
+//	float a, b, c, delta, x1, x2;
+//	printf("nhap a, b, c: ");
+//	scanf("%f %f %f", &a, &b, &c);
+//	delta = b * b - 4 * a * c;
+//	if(delta > 0){
+//		x1 = (-b + sqrt(delta)) / (2 * a); 
+//		x2 = (-b - sqrt(delta)) / (2 * a); 
+//		printf("nghiem cua phuong trinh la: x1 = %.2f, x2 = %.2f", x1, x2); 
+//	} 
+//	else if(delta == 0){
+//		x1 = x2 = -b / (2 * a);
+//		printf("nghiem cua phuong trinh la: x1 = x2 = %.2f", x1); 
+//	} 
+//	else{
+//		printf("phuong trinh vo nghiem"); 
+//	} 
+	//nhap vao 3 so a b c. kiem tra co phai la 3 canh cua tam giac khong
+//	float a, b, c; 
+//	printf("nhap vao a b c: ");
+//	scanf("%f %f %f", &a, &b, &c);
+//	if((a + b > c) && (a + c > b) && (a > 0) && (b > 0) &&(c > 0)) {
+//		printf("a b c la canh cua mot tam giac\n");
+//	} else {
+//		printf("a b c khong phai la canh cua mot tam giac: \n"); 
+//	} 
+	return 0;
+}
